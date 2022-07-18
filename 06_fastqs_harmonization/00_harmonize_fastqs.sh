23532ce87d63bc6653b7ecad8489bb905fd55373
Izaskun Mallona <izaskun.mallona@gmail.com>
2022-07-18 10:09:12 +0200

    Documentation, untested

--- 06_fastqs_harmonization/00_harmonize_fastqs.sh
+++ 06_fastqs_harmonization/00_harmonize_fastqs.sh
@@ -20,11 +20,11 @@
 # A regex might look like
 # '([ACTGN]{0,3})([ACTGN]{9})(GTGA|AATG{1})([ACTGN]{9})(GACA|CCAC){1}([ACTGN]{9})([ACTGN]{8})(.*)'
 
-WD=/home/imallona/tmp/regex
+WD=/home/imallona/tmp/regex_more_buckets
 DATA=/home/gmoro/fastqs_six_scRNAseq
 
 mkdir -p $WD; cd $WD
-zcat "$DATA"/20220629.B-o2875511-SPECTRAL_1_R1.fastq.gz | head -100 > test.fq
+# zcat "$DATA"/20220629.B-o2875511-SPECTRAL_1_R1.fastq.gz | head -100 > test.fq
 
 
 # what about chunking each R1/R2 file into a multiple-of-4 number of lines, and then running the parsing script?
@@ -63,7 +63,7 @@ N=32
         nice -n 19 /usr/local/R/R-4.1.0/bin/Rscript ~/src/ebrunner_spectral/06_fastqs_harmonization/01_harmonize_fastqs.R \
             -r1 "$r1" \
             -r2 "$r2" \
-            -o delete_me_chunked/"$curr" &
+            -o "$WD"/delete_me_chunked/"$curr" &
 
     done
 )
@@ -71,15 +71,15 @@ N=32
 
 ## ran till here
 
-mkdir -p output
+# mkdir -p output
 
-for item in non_matching AATG_CCAC_linkers GTGA_GACA_linkers
-do
-    find delete_me_chunked -name "$item*R2.fastq.gz" | sort | xargs zcat | \
-        gzip -c > output/grouped_"$item"_R2.fastq.gz
-    find delete_me_chunked -name "$item*R1.fastq.gz" | sort | xargs zcat | \
-        gzip -c > output/grouped_"$item"_R1.fastq.gz
-done
+# for item in non_matching AATG_CCAC_linkers GTGA_GACA_linkers
+# do
+#     find delete_me_chunked -name "$item*R2.fastq.gz" | sort | xargs zcat | \
+#         gzip -c > output/grouped_"$item"_R2.fastq.gz
+#     find delete_me_chunked -name "$item*R1.fastq.gz" | sort | xargs zcat | \
+#         gzip -c > output/grouped_"$item"_R1.fastq.gz
+# done
 
-rm *part*gz
-rm -rf delete_me_chunked
+# rm *part*gz
+# rm -rf delete_me_chunked
