4fdebac356b7da0e1d338b1d360bda963dc5b2e7
Izaskun Mallona <izaskun.mallona@gmail.com>
2022-07-21 13:27:10 +0200

    Add unmod file loop

--- 06_fastqs_harmonization/00_harmonize_fastqs.sh
+++ 06_fastqs_harmonization/00_harmonize_fastqs.sh
@@ -17,6 +17,10 @@
 # TCA   - seq1 9nt - linker1 4nt -  seq2 9nt - linker2 4nt - seq3 9nt - UMI 8 nt - T 25nt
 # TCGA  - seq1 9nt - linker1 4nt -  seq2 9nt - linker2 4nt - seq3 9nt - UMI 8 nt - TATGCGTAGTAGGTATG
 
+
+### modified sample
+
+
 # A regex might look like
 # '([ACTGN]{0,3})([ACTGN]{9})(GTGA|AATG{1})([ACTGN]{9})(GACA|CCAC){1}([ACTGN]{9})([ACTGN]{8})(.*)'
 ID=20220629.B-o2875511-SPECTRAL_1
@@ -70,18 +74,100 @@ N=32
     done
 )
 
+mkdir -p output
+
+for item in AATG_CCAC_else \
+                AATG_CCAC_Tn \
+                AATG_CCAC_TSO \
+                GTGA_GACA_else \
+                GTGA_GACA_Tn \
+                GTGA_GACA_TSO \
+                non_matching \
+                other_linkers_else \
+                other_linkers_Tn \
+                other_linkers_TSO
+do
+    find harmonized -name "$item*R2.fastq.gz" | sort | xargs zcat | \
+        gzip -c > output/grouped_"$item"_R2.fastq.gz
+    find harmonized -name "$item*R1.fastq.gz" | sort | xargs zcat | \
+        gzip -c > output/grouped_"$item"_R1.fastq.gz
+done
+
+# rm part*gz
+find . -name "part*gz" -delete
+rm -rf harmonized
+
+
+
+### unmodified sample ####################3
+
+
+
+
+
+# A regex might look like
+# '([ACTGN]{0,3})([ACTGN]{9})(GTGA|AATG{1})([ACTGN]{9})(GACA|CCAC){1}([ACTGN]{9})([ACTGN]{8})(.*)'
+ID=20220629.B-o2875514-SPECTRAL_4
+WD=/home/imallona/ebrunner_spectral/harmonize_fastqs/"$ID"
+DATA=/home/gmoro/fastqs_six_scRNAseq
+
+mkdir -p $WD; cd $WD
+
+mkdir -p "$WD"/harmonized
+
+NLINES=5000000
+
+zcat "$DATA"/"$ID"_R1.fastq.gz | split - -l "$NLINES" --filter='gzip > $FILE.r1.gz' part.
+zcat "$DATA"/"$ID"_R2.fastq.gz  | split - -l "$NLINES" --filter='gzip > $FILE.r2.gz' part.
+
+N=10
+
+(
+    for r1 in $(find . -name "part.*.r1.gz" | xargs -n"$N")
+    # for r1 in $(find . -name "part.dk.r1.gz" | xargs -n"$N") 
+    do 
+        ((i=i%N)); ((i++==0)) && wait
+        echo $r1
+        curr=$(basename $r1 .r1.gz)
+        r2="$curr".r2.gz
+        echo $r2
+    
+        echo $i
+        
+        nice -n 19 /usr/local/R/R-4.1.0/bin/Rscript ~/src/ebrunner_spectral/06_fastqs_harmonization/01_harmonize_fastqs.R \
+            -r1 "$r1" \
+            -r2 "$r2" \
+            -o "$WD"/harmonized/"$curr" &
+
+    done
+)
+
 
 ## ran till here
 
-# mkdir -p output
+mkdir -p output
 
-# for item in non_matching AATG_CCAC_linkers GTGA_GACA_linkers
-# do
-#     find delete_me_chunked -name "$item*R2.fastq.gz" | sort | xargs zcat | \
-#         gzip -c > output/grouped_"$item"_R2.fastq.gz
-#     find delete_me_chunked -name "$item*R1.fastq.gz" | sort | xargs zcat | \
-#         gzip -c > output/grouped_"$item"_R1.fastq.gz
-# done
+for item in AATG_CCAC_else \
+                AATG_CCAC_Tn \
+                AATG_CCAC_TSO \
+                GTGA_GACA_else \
+                GTGA_GACA_Tn \
+                GTGA_GACA_TSO \
+                non_matching \
+                other_linkers_else \
+                other_linkers_Tn \
+                other_linkers_TSO
+do
+    find harmonized -name "$item*R2.fastq.gz" | sort | xargs zcat | \
+        gzip -c > output/grouped_"$item"_R2.fastq.gz
+    find harmonized -name "$item*R1.fastq.gz" | sort | xargs zcat | \
+        gzip -c > output/grouped_"$item"_R1.fastq.gz
+done
 
-# rm *part*gz
-# rm -rf delete_me_chunked
+# rm part*gz
+find "$WD" -name "part*gz" -delete
+rm -rf "$WD"/harmonized
+
+
+
+
