203483088ee1f672fa0670fcdf67366ab668a425
Izaskun Mallona <izaskun.mallona@gmail.com>
2022-09-05 09:41:44 +0200

    On harmonizing a new dataset; committing lost changes from the zumis grid run (not tested nor run - yet)

--- 06_fastqs_harmonization/00_harmonize_fastqs.sh
+++ 06_fastqs_harmonization/00_harmonize_fastqs.sh
@@ -108,7 +108,7 @@ NLINES=5000000
 zcat "$DATA"/"$ID"_R1.fastq.gz | split - -l "$NLINES" --filter='gzip > $FILE.r1.gz' part.
 zcat "$DATA"/"$ID"_R2.fastq.gz  | split - -l "$NLINES" --filter='gzip > $FILE.r2.gz' part.
 
-N=10
+N=9
 
 (
     for r1 in $(find . -name "part.*.r1.gz" | xargs -n"$N")
@@ -129,9 +129,6 @@ N=10
 
     done
 )
-
-
-## ran till here
 
 mkdir -p output
 
@@ -159,3 +156,196 @@ rm -rf "$WD"/harmonized
 
 
 
+### another dataset
+
+ID=20220629.B-o2875513-SPECTRAL_3
+
+WD=/home/imallona/ebrunner_spectral/harmonize_fastqs/"$ID"
+DATA=/home/gmoro/fastqs_six_scRNAseq
+
+mkdir -p $WD; cd $WD
+
+mkdir -p "$WD"/harmonized
+
+NLINES=5000000
+
+zcat "$DATA"/"$ID"_R1.fastq.gz | split - -l "$NLINES" --filter='gzip > $FILE.r1.gz' part.
+zcat "$DATA"/"$ID"_R2.fastq.gz  | split - -l "$NLINES" --filter='gzip > $FILE.r2.gz' part.
+
+N=9
+
+(
+    for r1 in $(find . -name "part.*.r1.gz" | xargs -n"$N")
+    # for r1 in $(find . -name "part.dk.r1.gz" | xargs -n"$N") 
+    do 
+        ((i=i%N)); ((i++==0)) && wait
+        echo $r1
+        curr=$(basename $r1 .r1.gz)
+        r2="$curr".r2.gz
+        echo $r2
+    
+        echo $i
+        
+        nice -n 19 /usr/local/R/R-4.1.0/bin/Rscript ~/src/ebrunner_spectral/06_fastqs_harmonization/01_harmonize_fastqs.R \
+            -r1 "$r1" \
+            -r2 "$r2" \
+            -o "$WD"/harmonized/"$curr" &
+
+    done
+)
+
+mkdir -p output
+
+for item in AATG_CCAC_else \
+                AATG_CCAC_Tn \
+                AATG_CCAC_TSO \
+                GTGA_GACA_else \
+                GTGA_GACA_Tn \
+                GTGA_GACA_TSO \
+                non_matching \
+                other_linkers_else \
+                other_linkers_Tn \
+                other_linkers_TSO
+do
+    find harmonized -name "$item*R2.fastq.gz" | sort | xargs zcat | \
+        gzip -c > output/grouped_"$item"_R2.fastq.gz
+    find harmonized -name "$item*R1.fastq.gz" | sort | xargs zcat | \
+        gzip -c > output/grouped_"$item"_R1.fastq.gz
+done
+
+
+## ran till here
+
+
+
+
+## last, the x2 release
+
+
+### another dataset
+
+ID=20220629.B-o2875512-SPECTRAL_2
+
+WD=/home/imallona/ebrunner_spectral/harmonize_fastqs/"$ID"
+DATA=/home/gmoro/fastqs_six_scRNAseq
+
+mkdir -p $WD; cd $WD
+
+mkdir -p "$WD"/harmonized
+
+NLINES=5000000
+
+zcat "$DATA"/"$ID"_R1.fastq.gz | split - -l "$NLINES" --filter='gzip > $FILE.r1.gz' part.
+zcat "$DATA"/"$ID"_R2.fastq.gz  | split - -l "$NLINES" --filter='gzip > $FILE.r2.gz' part.
+
+N=9
+
+(
+    for r1 in $(find . -name "part.*.r1.gz" | xargs -n"$N")
+    # for r1 in $(find . -name "part.dk.r1.gz" | xargs -n"$N") 
+    do 
+        ((i=i%N)); ((i++==0)) && wait
+        echo $r1
+        curr=$(basename $r1 .r1.gz)
+        r2="$curr".r2.gz
+        echo $r2
+    
+        echo $i
+        
+        nice -n 19 /usr/local/R/R-4.1.0/bin/Rscript ~/src/ebrunner_spectral/06_fastqs_harmonization/01_harmonize_fastqs.R \
+            -r1 "$r1" \
+            -r2 "$r2" \
+            -o "$WD"/harmonized/"$curr" &
+
+    done
+)
+
+mkdir -p output
+
+for item in AATG_CCAC_else \
+                AATG_CCAC_Tn \
+                AATG_CCAC_TSO \
+                GTGA_GACA_else \
+                GTGA_GACA_Tn \
+                GTGA_GACA_TSO \
+                non_matching \
+                other_linkers_else \
+                other_linkers_Tn \
+                other_linkers_TSO
+do
+    find harmonized -name "$item*R2.fastq.gz" | sort | xargs zcat | \
+        gzip -c > output/grouped_"$item"_R2.fastq.gz
+    find harmonized -name "$item*R1.fastq.gz" | sort | xargs zcat | \
+        gzip -c > output/grouped_"$item"_R1.fastq.gz
+done
+
+
+
+
+## new dataset 5th sept 2022
+
+
+
+
+
+WD=/home/imallona/ebrunner_spectral/harmonize_fastqs/"$ID"
+DATA=/home/gmoro/fastqs_six_scRNAseq_separated
+
+
+mkdir -p $WD; cd $WD
+
+mkdir -p "$WD"/harmonized
+
+for fastq in $(find $DATA -name "*R1.fastq.gz")
+do
+    echo $fastq
+    ID=$(basename $fastq _R1.fastq.gz)
+
+    echo $ID
+    
+    NLINES=5000000
+
+    zcat "$DATA"/"$ID"_R1.fastq.gz | split - -l "$NLINES" --filter='gzip > $FILE.r1.gz' part.
+    zcat "$DATA"/"$ID"_R2.fastq.gz  | split - -l "$NLINES" --filter='gzip > $FILE.r2.gz' part.
+
+    N=10
+
+    (
+        for r1 in $(find . -name "part.*.r1.gz" | xargs -n"$N")
+                  # for r1 in $(find . -name "part.dk.r1.gz" | xargs -n"$N") 
+        do 
+            ((i=i%N)); ((i++==0)) && wait
+            echo $r1
+            curr=$(basename $r1 .r1.gz)
+            r2="$curr".r2.gz
+            echo $r2
+            
+            echo $i
+            
+            nice -n 19 /usr/local/R/R-4.1.0/bin/Rscript ~/src/ebrunner_spectral/06_fastqs_harmonization/01_harmonize_fastqs.R \
+                 -r1 "$r1" \
+                 -r2 "$r2" \
+                 -o "$WD"/harmonized/"$curr" &
+
+        done
+    )
+
+    mkdir -p output
+
+    for item in AATG_CCAC_else \
+                    AATG_CCAC_Tn \
+                    AATG_CCAC_TSO \
+                    GTGA_GACA_else \
+                    GTGA_GACA_Tn \
+                    GTGA_GACA_TSO \
+                    non_matching \
+                    other_linkers_else \
+                    other_linkers_Tn \
+                    other_linkers_TSO
+    do
+        find harmonized -name "$item*R2.fastq.gz" | sort | xargs zcat | \
+            gzip -c > output/grouped_"$item"_R2.fastq.gz
+        find harmonized -name "$item*R1.fastq.gz" | sort | xargs zcat | \
+            gzip -c > output/grouped_"$item"_R1.fastq.gz
+    done
+done
--- 09_zumis_grid/00_run_zumis_grid.sh
+++ 09_zumis_grid/00_run_zumis_grid.sh
@@ -15,12 +15,14 @@ WD=/home/imallona/ebrunner_spectral/zumis_systematic
 DATA=/home/imallona/ebrunner_spectral/harmonize_fastqs
 OUT="$WD"/out
 
+set +e
+
 mkdir -p $WD
 cd $WD
 
 mkdir -p data
 
-cat << EOF > data/gfp_and_tso.fa
+cat << EOF > data/gfp_and_others.fa
 >egfp
 ATGGTGAGCAAGGGCGAGGAGCTGTTCACCGGGGTGGTGCCCATCCTGGTCGAGCTGGACGGCGACGTAA
 ACGGCCACAAGTTCAGCGTGTCCGGCGAGGGCGAGGGCGATGCCACCTACGGCAAGCTGACCCTGAAGTT
@@ -34,7 +36,9 @@ CCCCATCGGCGACGGCCCCGTGCTGCTGCCCGACAACCACTACCTGAGCACCCAGTCCGCCCTGAGCAAA
 GACCCCAACGAGAAGCGCGATCACATGGTCCTGCTGGAGTTCGTGACCGCCGCCGGGATCACTCTCGGCA
 TGGACGAGCTGTACAAGTAA
 >tso_oligomer
-TATGCGTAGTAGGTATGTATGCGTAGTAGGTATGTATGCGTAGTAGGTATGTATGCGTAGTAGGTATG
+TATGCGTAGTAGGTATGTATGCGTAGTAGGTATGTATGCGTAGTAGGTATGTATGCGTAGTAGGTATGTATGCGTAGTAGGTATGTATGCGTAGTAGGTATGTATGCGTAGTAGGTATGTATGCGTAGTAGGTATGTATGCGTAGTAGGTATGTATGCGTAGTAGGTATGTATGCGTAGTAGGTATGTATGCGTAGTAGGTATG
+>n1_oligomer
+ACAGGAAACTCATGGTGCGTACAGGAAACTCATGGTGCGTACAGGAAACTCATGGTGCGTACAGGAAACTCATGGTGCGTACAGGAAACTCATGGTGCGTACAGGAAACTCATGGTGCGTACAGGAAACTCATGGTGCGTACAGGAAACTCATGGTGCGTACAGGAAACTCATGGTGCGTACAGGAAACTCATGGTGCGTACAGGAAACTCATGGTGCGTACAGGAAACTCATGGTGCGT
 EOF
 
 
@@ -55,47 +59,58 @@ EOF
  
 # downsampling, how much?
 
+
+source /home/imallona/virtenvs/zumis/bin/activate 
+
+export R_LIBS=/home/imallona/R/x86_64-pc-linux-gnu-library/3.6/
+
+
 for run in $(find $DATA -maxdepth 1 -mindepth 1 -type d)
 do
     echo "$run start"
 
     run_id=$(basename $run)
 
-    for bucket in grouped_AATG_CCAC_else \
-                      grouped_AATG_CCAC_Tn \
-                      grouped_AATG_CCAC_TSO \
-                      grouped_GTGA_GACA_else \
-                      grouped_GTGA_GACA_Tn \
-                      grouped_GTGA_GACA_TSO \
-                      grouped_non_matching \
-                      grouped_other_linkers_else \
-                      grouped_other_linkers_Tn \
-                      grouped_other_linkers_TSO
+    # for bucket in grouped_AATG_CCAC_else \
+    #                   grouped_AATG_CCAC_Tn \
+    #                   grouped_AATG_CCAC_TSO \
+    #                   grouped_GTGA_GACA_else \
+    #                   grouped_GTGA_GACA_Tn \
+    #                   grouped_GTGA_GACA_TSO \
+    #                   grouped_non_matching \
+    #                   grouped_other_linkers_else \
+    #                   grouped_other_linkers_Tn \
+    #                   grouped_other_linkers_TSO
+
+    for bucket in grouped_AATG_CCAC_TSO \
+                      grouped_GTGA_GACA_Tn
     do
 
-        for external_seqs in gfp_and_tso gfp
+        for external_seqs in gfp_and_others gfp
         do
             
-            for cb_hamming in 0 1 2 3
+            for cb_hamming in 0 4
             do
-                curr_outdir="$OUT"/"$run_id"/"$bucket"/"$external_seqs"/cb_hamming_"$cb_hamming"
-                mkdir -p "$curr_outdir"
-                cd "$curr_outdir"
+                for primary_hit in yes no
+                do
+                    curr_outdir="$OUT"/"$run_id"/"$bucket"/"$external_seqs"/cb_hamming_"$cb_hamming"/primary_hit_"$primary_hit"
+                    mkdir -p "$curr_outdir"
+                    cd "$curr_outdir"
 
-                curr="$run_id"_"$bucket"_"$external_seqs"_cb_hamming_"$cb_hamming"
-                
-                echo "$run $bucket $external_seq $cb_hamming start"
+                    curr="$run_id"_"$bucket"_"$external_seqs"_cb_hamming_"$cb_hamming"_primary_hit_"$primary_hit"
+                    
+                    echo "$run $bucket $external_seq $cb_hamming $primary_hit start"
               
                 cat <<EOF > "$curr".yaml
 project: $curr
 sequence_files:
   file1:
-    name: $run/output/$bucket_R1.fastq.gz
+    name: $run/output/${bucket}_R1.fastq.gz
     base_definition:
     - BC(1-9,14-22,27-35)
     - UMI(36-43)
   file2:
-    name: $run/output/$bucket_R2.fastq.gz
+    name: $run/output/${bucket}_R2.fastq.gz
     base_definition: cDNA(1-62)
 reference:
   STAR_index: /home/gmoro/imallona2giulia/indices/GRCm38_gencode_M25
@@ -125,7 +140,7 @@ counting_opts:
   strand: 0
   Ham_Dist: 0                ## no Hamming for UMIs
   velocyto: no
-  primaryHit: no             ## get multimappers
+  primaryHit: $primary_hit             ## get multimappers
   twoPass: yes               ## two pass mapping
 make_stats: yes
 which_Stage: Filtering
@@ -138,11 +153,34 @@ EOF
 
                 echo $curr
                 echo 'run me'
+                nice -n 19 bash /home/imallona/soft/zumis_alt/zUMIs/zUMIs.sh \
+                     -y "$curr".yaml | tee > "$curr_outdir"/zumis_run.log
+                
                 echo 'immediately remove bam files to save space'
                 cd $WD
-                
+                done
             done
         done
-    done
-        
+    done       
 done
+
+
+# test
+
+
+run=/home/imallona/ebrunner_spectral/harmonize_fastqs/20220629.B-o2875514-SPECTRAL_4
+run_id=$(basename $run)
+
+bucket=grouped_AATG_CCAC_TSO
+external_seqs=gfp_and_others
+cb_hamming=0
+primary_hit=no
+
+curr="$run_id"_"$bucket"_"$external_seqs"_cb_hamming_"$cb_hamming"_primary_hit_"$primary_hit"
+                  
+curr_outdir="$OUT"/"$run_id"/"$bucket"/"$external_seqs"/cb_hamming_"$cb_hamming"/primary_hit_"$primary_hit"
+mkdir -p "$curr_outdir"
+cd "$curr_outdir"
+
+nice -n 19 bash /home/imallona/soft/zumis_alt/zUMIs/zUMIs.sh \
+     -y "$curr".yaml | tee > "$curr_outdir"/zumis_run.log
